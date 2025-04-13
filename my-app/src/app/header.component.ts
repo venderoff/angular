152f1b78e08147b55a3f028355579d179ac8c831
venderoff <venderoff@gmail.com>
2025-04-13 12:54:25 +0530

    add new component user

---
+++ my-app/src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+selector: 'app-header',
+standalone: true,
+templateUrl: './header.component.html',
+styleUrl: './app.component.css'
+
+})
+export class HeaderComponent{}
